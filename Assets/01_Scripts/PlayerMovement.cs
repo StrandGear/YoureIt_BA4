3cb7628e6e1fbe0fcb298850097365bf56205bab
Rekamae <maurekac@gmail.com>
2024-06-27 19:01:26 +0200

    Working on crouching

--- Assets/01_Scripts/PlayerMovement.cs
+++ Assets/01_Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -17,16 +18,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float maxCameraClamp; 
     [SerializeField] private GameObject mainCam;
     
+    
     [SerializeField] private float playerSpeed = 2.0f;
     [SerializeField] private float speedBoostMultiplier = 2.0f; 
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
-    [SerializeField] private float crouch;
     
-    //[SerializeField] private float rotationSpeed = 4f;
+    [SerializeField] private float crouchSpeed;
     
     
-
     private float climbingTimer;
     
     private CharacterController controller;
@@ -34,25 +34,33 @@ public class PlayerMovement : MonoBehaviour
     private bool groundedPlayer;
     private bool isClimbing;
     private bool isStartingToClimb;
+    private bool isCrouching;
+    private bool canStand;
     private Transform cameraMainTransform;
     private float xRotation;
     private float yRotation;
-    //private Animator animator;
     
+    private Vector3 originalCenter;
+    private float originalHeight;
+    private float originalRadius;
+    //private Animator animator;
 
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
-        //animator = GetComponent<Animator>();
-        /*cameraMainTransform = Camera.main.transform;*/
+        cameraMainTransform = Camera.main.transform;
+
+        originalCenter = controller.center;
+        originalHeight = controller.height;
+        originalRadius = controller.radius;
     }
 
     void Update()
     {
         UpdateGroundedStatus();
         HandleMovement();
+        HandleCrouching(); // Call HandleCrouching in Update
         HandleClimbingTransition();
-        print("Test");
     }
 
     private void LateUpdate()
@@ -60,27 +68,25 @@ public class PlayerMovement : MonoBehaviour
         CameraRotation();
     }
 
-
     private void UpdateGroundedStatus()
     {
-        //print(transform.position.y);
         var ray = new Ray(transform.position, Vector3.down);
         groundedPlayer = Physics.Raycast(ray, 0.1f, groundLayer);
-
-        
     }
 
     private void HandleMovement()
     {
-        
         Vector2 movement = movementControl.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(movement.x, 0, movement.y);
-        /*move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;*/
-        float targetRotation = 0;
+        move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
         move.y = 0;
 
         float currentSpeed = playerSpeed;
-        if (speedBoostControl.action.IsPressed())
+        if (isCrouching)
+        {
+            currentSpeed = crouchSpeed;
+        }
+        if (speedBoostControl.action.IsPressed() && !isCrouching)
         {
             currentSpeed *= speedBoostMultiplier;
         }
@@ -93,7 +99,6 @@ public class PlayerMovement : MonoBehaviour
         {
             HandleWalkingAndJumping(move, currentSpeed);
         }
-
         
         UpdateClimbingTimer();
     }
@@ -111,21 +116,17 @@ public class PlayerMovement : MonoBehaviour
             !raycastHit.transform.CompareTag("Ladder") || (groundedPlayer && !isStartingToClimb))
         {
             isClimbing = false;
-            //animator.SetBool("Climbing", false);
         }
     }
 
     private void HandleWalkingAndJumping(Vector3 move, float currentSpeed)
     {
-        
-
         if (jumpControl.action.triggered && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
             Debug.Log("Player Jumped");
         }
-        
-        
+
         if (groundedPlayer && playerVelocity.y <= 0)
         {
             playerVelocity.y = 0f;
@@ -136,10 +137,8 @@ public class PlayerMovement : MonoBehaviour
         }
 
         float targetRotation;
-        
         if (move != Vector3.zero && !isClimbing)
         {
-            
             targetRotation = Quaternion.LookRotation(move).eulerAngles.y + mainCam.transform.rotation.eulerAngles.y;
             Quaternion rotation = Quaternion.Euler(0, targetRotation, 0);
             move = rotation * move;
@@ -148,30 +147,39 @@ public class PlayerMovement : MonoBehaviour
 
         var localMove = move * currentSpeed * Time.deltaTime;
         controller.Move(localMove + playerVelocity * Time.deltaTime);
-        
-        //controller.Move(baseMove + playerVelocity * Time.deltaTime);
-        print("Gravity");
     }
 
-    /*private void RotatePlayer(Vector2 movement)
+    private void HandleCrouching()
     {
-        float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg /*+ cameraMainTransform.eulerAngles.y#1#;
-        Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-    }*/
-    
+        if (crouchControl.action.triggered)
+        {
+            if (isCrouching)
+            {
+                isCrouching = false;
+                jumpHeight = 1.0f;
+                controller.center = originalCenter;
+                controller.height = originalHeight;
+                controller.radius = originalRadius;
+            }
+            else
+            {
+                isCrouching = true;
+                jumpHeight = 0f;
+                controller.height = 0.5f;
+            }
+        }
+    }
 
     private void CameraRotation()
     {
         Vector2 lookAround = lookControl.action.ReadValue<Vector2>();
-        
+
         xRotation -= lookAround.y;
         yRotation += lookAround.x;
         xRotation = Mathf.Clamp(xRotation, minCameraClamp, maxCameraClamp);
         Quaternion camRotation = Quaternion.Euler(xRotation, yRotation, 0);
         cameraFollowTarget.rotation = camRotation;
     }
-    
 
     private void UpdateClimbingTimer()
     {
@@ -201,8 +209,6 @@ public class PlayerMovement : MonoBehaviour
                     isStartingToClimb = true;
                     climbingTimer = 2f;
                     playerVelocity.y = 0f;
-                    print("Start Climbing");
-                    //animator.SetBool("Climbing", true);
                 }
             }
         }
@@ -212,19 +218,19 @@ public class PlayerMovement : MonoBehaviour
     {
         movementControl.action.Enable();
         jumpControl.action.Enable();
-        speedBoostControl.action.Enable(); 
-        interactControl.action.Enable(); 
-        lookControl.action.Enable(); 
+        speedBoostControl.action.Enable();
+        interactControl.action.Enable();
+        lookControl.action.Enable();
         crouchControl.action.Enable();
     }
-    
+
     private void OnDisable()
     {
         movementControl.action.Disable();
         jumpControl.action.Disable();
-        speedBoostControl.action.Disable(); 
-        interactControl.action.Disable(); 
-        lookControl.action.Disable(); 
+        speedBoostControl.action.Disable();
+        interactControl.action.Disable();
+        lookControl.action.Disable();
         crouchControl.action.Disable();
     }
 }
