796146490ed664f19ab905c31a8454bc14f8d32e
ercee1 <137692271+ercee1@users.noreply.github.com>
2024-07-14 15:03:32 +0200

    upd

---
+++ Assets/Flickering.cs
@@ -0,0 +1,28 @@
+//	Name			Chau Tran
+//	Last Modified	Feb 7th,2015
+
+using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+	
+	Light testLight;
+	public float minWaitTime;
+	public float maxWaitTime;
+	
+	void Start () {
+		testLight = GetComponent<Light>();
+		StartCoroutine(Flashing());
+	}
+	
+	IEnumerator Flashing ()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(minWaitTime,maxWaitTime));
+			testLight.enabled = ! testLight.enabled;
+
+		}
+	}
+}
