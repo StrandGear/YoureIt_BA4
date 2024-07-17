2b70557480e89fcb8b687fda2515bf07c693c74e
StrandGear <strandgear@mail.ru>
2024-07-18 01:01:34 +0200

    Merge branch 'main' into Felix

--- Assets/01_Scripts/Enemy/EnemyFollow.cs
+++ Assets/01_Scripts/Enemy/EnemyFollow.cs
@@ -1,17 +1,17 @@
 using System;
 using UnityEngine;
 
-public class EnemyFollow
-    : MonoBehaviour
+public class EnemyFollow : MonoBehaviour
 {
-    [SerializeField] Transform target; // The player or target to follow
-    [SerializeField] float rangeToFollow = 10f; // Range within which the enemy starts following
-    [SerializeField] float attackRange = 2f; // Range within which the enemy starts attacking
-    [SerializeField] float moveSpeed = 5f; // Movement speed when following
-    [SerializeField] float attackingSpeed = 7f; // Movement speed when attacking
-    [SerializeField]  PlayerRespawn playerRespawn; 
-    [SerializeField]  float respawnDistance; 
-
+    [SerializeField] Transform target; 
+    [SerializeField] float rangeToFollow = 10f; 
+    [SerializeField] float attackRange = 2f; 
+    [SerializeField] float moveSpeed = 5f; 
+    [SerializeField] float attackingSpeed = 7f;
+    [SerializeField] float speedUpDistance = 20f; // Distance to speed up
+    [SerializeField] float respawnDistance;
+    [SerializeField] PlayerRespawn playerRespawn; 
+     
     private Vector3 targetStoredPosition; 
     private bool attack = false; 
     private bool isFollowingThePlayer = false; 
@@ -35,39 +35,23 @@ public class EnemyFollow
 
     void Update()
     {
-        var targetPosition = target.position + Vector3.right * rangeToFollow;
-        var difference = targetPosition - transform.position;
-        var movement = difference.normalized * moveSpeed * Time.deltaTime;
-        transform.position += movement;
-        var pos = transform.position;
-        transform.position = new Vector3(pos.x, targetPosition.y, pos.z);
-        /*distance = Vector3.Distance(transform.position, target.position);
+        float currentSpeed = moveSpeed;
+        distance = Vector3.Distance(transform.position, target.position + Vector3.right * rangeToFollow);
 
-        if (!attack)
+        if (distance > speedUpDistance)
         {
-            if (distance < rangeToFollow)
-            {
-                isFollowingThePlayer = true;
-                transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-                targetStoredPosition = target.position;
-
-                //Debug.Log("Player's previous position is: " + targetStoredPosition);
-
-                if (distance < attackRange)
-                {
-                    attack = true;
-                }
-            }
+            currentSpeed = attackingSpeed;
         }
         else
         {
-            isFollowingThePlayer = false;
-            transform.position = Vector3.MoveTowards(transform.position, targetStoredPosition, attackingSpeed * Time.deltaTime);
+            currentSpeed = moveSpeed;
+        }
 
-            if (transform.position == targetStoredPosition)
-            {
-                attack = false;
-            }
-        }*/
+        var targetPosition = target.position + Vector3.right * rangeToFollow;
+        var difference = targetPosition - transform.position;
+        var movement = difference.normalized * currentSpeed * Time.deltaTime;
+        transform.position += movement;
+        var pos = transform.position;
+        transform.position = new Vector3(pos.x, targetPosition.y, pos.z);
     }
 }
