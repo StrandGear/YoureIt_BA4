fa4a7a2bd30b7e89cc764cfd064621e5f0788705
StrandGear <strandgear@mail.ru>
2024-07-17 13:32:24 +0200

    Added audio tracks

--- Assets/07_Audio/FMODEvents.cs
+++ Assets/07_Audio/FMODEvents.cs
@@ -10,10 +10,24 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference ambience { get; set; }
 
     [field: Header("Player SFX")]
-    [field: SerializeField] public EventReference playerFootsteps {  get; set; }
+    [field: SerializeField] public List<EventReference> playerFootsteps {  get; set; }
 
     [field: Header("Eye SFX")]
-    [field: SerializeField] public EventReference eyeCollected { get; private set; }    
+    
+
+    [field: Header("UI SFX")]
+    [field: SerializeField] public EventReference UI_selectElement { get; private set; }
+    [field: SerializeField] public EventReference UI_puzzleSuccess { get; private set; }
+    [field: SerializeField] public EventReference UI_paperUnfolding { get; private set; }
+    [field: SerializeField] public EventReference UI_cantSelect { get; private set; }
+
+    [field: Header("General SFX")]
+    [field: SerializeField] public EventReference RussianDoll { get; private set; }
+    [field: SerializeField] public EventReference PlankFalling { get; private set; }
+    [field: SerializeField] public EventReference Keys { get; private set; }
+    [field: SerializeField] public EventReference Door { get; private set; }
+    [field: SerializeField] public EventReference BackpackZipper { get; private set; }
+    [field: SerializeField] public EventReference eyeCollected { get; private set; }
     public static FMODEvents instance { get; private set; }
 
     private void Awake()
