a7d32f54ac7e71b009b291ea21ba69288e38eee0
StrandGear <strandgear@mail.ru>
2024-07-24 12:16:52 +0200

    interaction unfixed again cause didnt work

--- Assets/01_Scripts/Interactables/InteractableObject.cs
+++ Assets/01_Scripts/Interactables/InteractableObject.cs
@@ -28,8 +28,6 @@ public class InteractableObject : MonoBehaviour
         interactionCanvasElement = transform.Find("GameCanvas").gameObject;
         interactionCanvasElement.GetComponentInChildren<TMP_Text>().text = interactionText;
 
-        if (gameObject.GetComponent<MeshRenderer>() != null)
-            gameObject.GetComponent<MeshRenderer>().enabled = false;
     }
     private void Start()
     {
